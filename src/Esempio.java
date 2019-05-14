2bad806244ebbbe8745ffe305f83f0dca0a528f6
4a <4a@PianoDue-05>
2019-05-14 13:12:53 +0200

    Versione 2

--- src/Esempio.java
+++ src/Esempio.java
@@ -1,6 +1,6 @@
-//Versione 1_
+//Versione 2
 public class Esempio {
 	public static void main(String[] args) {
-		System.out.println("Ciao GitHub!");
+		System.out.println("Ciao GitHub! ... 2");
 	}
 }
